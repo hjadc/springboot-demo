bb8ffcdd0455414417e47a614476b1a1610f7c4f
a_huju <v_huju@baidu.com>
2018-06-22 11:50:38 +0800

    提交

---
+++ src/main/java/com/huju/lol/controller/HelloWorldController.java
@@ -0,0 +1,7 @@
+package com.huju.lol.controller;
+
+/**
+ * Created by v_huju on 2018/6/21.
+ */
+public class HelloWorldController {
+}
---
+++ src/test/java/com/huju/lol/DemoApplicationTests.java
@@ -0,0 +1,7 @@
+package com.huju.lol;
+
+/**
+ * Created by v_huju on 2018/6/21.
+ */
+public class DemoApplicationTests {
+}
