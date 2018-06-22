65e32ff7e3d522311570eef26e90946e4d30e776
a_huju <v_huju@baidu.com>
2018-06-22 11:53:58 +0800

    提交

--- src/main/java/com/huju/lol/controller/HelloWorldController.java
+++ src/main/java/com/huju/lol/controller/HelloWorldController.java
@@ -1,7 +1,64 @@
 package com.huju.lol.controller;
 
+import com.huju.lol.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
 /**
  * Created by v_huju on 2018/6/21.
  */
+@RestController
+@RequestMapping("/users")
 public class HelloWorldController {
+
+    static Map<Long,User> userMap = Collections.synchronizedMap(new HashMap<>());
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "Spring Boot Hello World!";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute User user){
+        user.setId(1l);
+        user.setName("王老五");
+        user.setAge(28);
+        System.out.println(user.toString());
+        userMap.put(user.getId(), user);
+
+        return "success";
+    }
+
+
+    // http://localhost:8080/users/?page=1&size=10
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<User> getUserList(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer size){
+        System.out.println("page=" + page + " ,size=" + size);
+        List<User> users = new ArrayList<>(userMap.values());
+        return users;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return userMap.get(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String updateUser(@PathVariable Long id, @ModelAttribute User user){
+        User user1 = userMap.get(id);
+        user1.setName(user.getName());
+        user1.setAge(user.getAge());
+
+        userMap.put(id, user1);
+        return "success";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        userMap.remove(id);
+
+        return "success";
+    }
 }
--- src/test/java/com/huju/lol/DemoApplicationTests.java
+++ src/test/java/com/huju/lol/DemoApplicationTests.java
@@ -1,7 +1,83 @@
 package com.huju.lol;
 
+import com.huju.lol.controller.HelloWorldController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+
 /**
  * Created by v_huju on 2018/6/21.
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class DemoApplicationTests {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+    }
+
+    @Test
+    public void testHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Spring Boot Hello World!")));
+    }
+
+    @Test
+    public void testUserApi() throws Exception{
+        RequestBuilder request = null;
+
+        // get 获取所有用户列表
+        request = get("/users/");
+        mvc.perform(request).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+        // post 添加用户
+        request = post("/users/")
+                .param("id", "1")
+                .param("name", "mengday")
+                .param("age", "20");
+        mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+        // get get all users
+        request = get("/users/");
+        mvc.perform(request).andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"mengday\",\"age\":20}]")));
+
+
+        // put 更新用户
+        request = put("/users/1")
+                .param("name", "mengday")
+                .param("age", "28");
+        mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+        // 查看某个用户的信息
+        request = get("/users/1");
+        mvc.perform(request).andExpect(content().string(equalTo("{\\\"id\\\":1,\\\"name\\\":\\\"mengday\\\",\\\"age\\\":28}")));
+
+        // 删除用户信息
+        request = delete("/users/1");
+        mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+        request = get("/users/");
+        mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(equalTo("[]")));
+    }
 }
